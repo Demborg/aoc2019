3bb1eadd84126519b6e8dd33ce899243d4f1e7e9
Axel Demborg <axel.demborg@gmail.com>
2019-12-03 15:44:04 +0100

    solve the second part

--- 3.py
+++ 3.py
@@ -2,20 +2,30 @@ from sys import stdin
 
 direction = {'R': (1, 0), 'L': (-1, 0), 'U': (0, 1), 'D': (0, -1)}
 
-def make_set(row):
+def make_dict(row):
+    place = 1
     pos = (0, 0)
-    s = set()
+    s = {}
     for move in row.split(','):
         for i in range(int(move[1:])):
             pos = tuple(p + d for p, d in zip(pos, direction[move[0]]))
-            s.add(pos)
+            if not pos in s:
+                s[pos] = place
+            place+=1
     return s
 
-#p1
-s1 = make_set(stdin.readline())
-s2 = make_set(stdin.readline())
+s1 = make_dict(stdin.readline())
+s2 = make_dict(stdin.readline())
 
-print(sorted(sum(abs(v) for v in t) for t in s1.intersection(s2)))
+intersecting_points = set(s1.keys()).intersection(s2.keys())
+
+#p1
+print(sorted(sum(abs(v) for v in t) for t in intersecting_points))
+
+#p2
+print(sorted(s1[p] +s2[p] for p in intersecting_points))
+
+
 
 
         
