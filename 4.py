6cb0c7dbf529fc7334348530fde0ad054549a3c7
Axel Demborg <axel.demborg@gmail.com>
2019-12-05 14:31:36 +0100

    part 2 of 4

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
 print(len({
     s for s in (str(n) for n in range(240920, 789857))
-    if len(set(s)) < 6 
+    if any(2 == sum(s[i] == c for i in range(len(s))) for c in set(s))
     and all(int(s[i]) <= int(s[i+1]) for i in range(len(s) -1))
 }))
